0d528c6bd0d88cc4cea4a3cc02bc49e6fdee0847
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-01-01 15:14:43 -0600

    Add UserRepository interface

---
+++ src/main/java/api/springsecjwt/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package api.springsecjwt.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import api.springsecjwt.entities.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    User findByUsername(String username);
+}
