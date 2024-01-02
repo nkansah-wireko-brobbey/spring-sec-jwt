d7a16da29bc9a06c0d85a3e142f2fac33309a1d6
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-01-01 18:24:10 -0600

    Refactor User entity to implement UserDetails interface

--- src/main/java/api/springsecjwt/entities/User.java
+++ src/main/java/api/springsecjwt/entities/User.java
@@ -1,17 +1,28 @@
 package api.springsecjwt.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Column;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import lombok.Data;
 
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
-public class User {
+@Table(name = "users")
+public class User implements UserDetails{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,15 +34,47 @@ public class User {
     @Column(name = "last_name")
     private String lastName;
 
-    private String username;
 
     private String password;
 
     @Column(name = "role_type")
-    private Role roleType;
+    @Enumerated(EnumType.STRING)
+    private Role role;
 
     private String email;
 
+    private LocalDateTime createdAt;
 
-    
+    private LocalDateTime updatedAt;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of( new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
