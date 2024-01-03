9f76f4dc70313d7886fe56b0258e99883954f259
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-01-03 11:53:35 -0600

    Add UserDetailsService and save method to UserService

--- src/main/java/api/springsecjwt/services/UserService.java
+++ src/main/java/api/springsecjwt/services/UserService.java
@@ -1,2 +1,37 @@
-package api.springsecjwt.services;public class UserService {
+package api.springsecjwt.services;
+import api.springsecjwt.entities.User;
+import api.springsecjwt.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+    public UserDetailsService userDetailsService() {
+        return new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                return userRepository.findByEmail(username)
+                        .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+            }
+        };
+    }
+
+    public User save(User newUser) {
+
+        if (newUser.getId() != null) {
+            newUser.setCreatedAt(LocalDateTime.now());
+        }
+
+        newUser.setUpdatedAt(LocalDateTime.now());
+
+        return userRepository.save(newUser);
+    }
+
 }
