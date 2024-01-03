41a0867fedb3d7f1752b3782f97f62c79c88cf7e
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-01-03 11:50:46 -0600

    Add SeedDataConfig class for initializing admin user

---
+++ src/main/java/api/springsecjwt/config/SeedDataConfig.java
@@ -0,0 +1,43 @@
+package api.springsecjwt.config;
+
+
+import api.springsecjwt.entities.Role;
+import api.springsecjwt.entities.User;
+import api.springsecjwt.repositories.UserRepository;
+import api.springsecjwt.services.UserService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class SeedDataConfig implements CommandLineRunner {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final UserService userService;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        if (userRepository.count() == 0) {
+
+            User admin = User
+                    .builder()
+                    .firstName("admin")
+                    .lastName("admin")
+                    .email("admin@admin.com")
+                    .password(passwordEncoder.encode("password"))
+                    .role(Role.ROLE_ADMIN)
+                    .build();
+
+            userService.save(admin);
+            log.debug("created ADMIN user - {}", admin);
+        }
+    }
+
+}
