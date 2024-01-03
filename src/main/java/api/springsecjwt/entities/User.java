361921cba38996c8bbd3a934202eff4ff39b9c0c
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-01-03 11:53:01 -0600

    Update Role and User entity fields

--- src/main/java/api/springsecjwt/entities/User.java
+++ src/main/java/api/springsecjwt/entities/User.java
@@ -29,7 +29,7 @@ public class User implements UserDetails{
     private Integer id;
 
     @Column(name = "first_name")
-    private String firsName;
+    private String firstName;
 
     @Column(name = "last_name")
     private String lastName;
